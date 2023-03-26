cc1be0369136c7532e094d2e0ceaad4a1a661d92
Hordiyenko Tetyana <hordit@me.com>
2023-03-26 14:12:05 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -5,7 +5,7 @@ function fetchCountries(name) {
     return fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error();
+        throw new Error(response.status);
       }
        return response.json()
     })
